1f8a6b6ce74ce2e3e50d406b78de22a6470b36c8
Shiyan Artem <artem.shiyan68@gmail.com>
2021-01-19 01:17:44 +0300

    версия ОС и Python

---
+++ red_hat/01_os_info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Нужно собрать информациб об операционной системе и версии пайтона
+
+import platform
+import sys
+
+info = 'OS info is \n{}\n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
